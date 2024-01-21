760691f5ac4dde3a80c8fd68e031009a2330194e
Soaringxmc <maochao_xiao@outlook.com>
2024-01-21 22:49:08 +0100

    0

--- loop.sh
+++
@@ -1,7 +0,0 @@
-find . -type f -name "*.dat" | while read -r dir; do
-    # Remove each found file
-    file="${dir#./}"
-    echo ""
-    echo $file
-    git filter-repo --force --invert-paths --path-match $file
-done
