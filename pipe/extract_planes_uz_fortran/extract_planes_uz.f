a5a125a058fa172c3ffb5bfefd2b3d2b4b6f7f22
Soaringxmc <mchao@newton.dima.uniroma1.it>
2024-02-24 17:45:28 +0100

    field3d modified

--- pipe/extract_planes_uz_fortran/extract_planes_uz.f
+++ pipe/extract_planes_uz_fortran/extract_planes_uz.f
@@ -12,10 +12,36 @@
       real(dp), allocatable, dimension(:,:,:) :: planerz
       real(dp), allocatable, dimension(:,:,:) :: planetr,planetrl
       real(dp), allocatable, dimension(:) :: th,rc,rm,z,sth,cth,rr
+      real(dp), allocatable, dimension(:,:) :: wvarav
       integer :: jplanes(nplanes)
       character(1) :: naplane
+      character(100) :: napltr,napltz,naplrz
+      logical :: lfluc,luz,luth,lur
+      integer :: lnd
+
+!     instantaneous fields
+      lfluc = .false.
+!     output of uz
+      luz = .true.
+!     output of uth
+      luth =.false.       
+!     output of ur
+      lur = .false.
+!     nondimensionalized by up(0),ub(1),utau(2)
+      lnd = 1
+!     it is impossible to average to cell centers
+!     e.g. ur on planetz 
+      if (lfluc) then
+       napltr = 'planetr_fluc' 
+       napltz = 'planetz_fluc_'
+       naplrz = 'planerz_fluc'
+      else
+       napltr = 'planetr' 
+       napltz = 'planetz_'
+       naplrz = 'planerz'
+      endif
 !
-!     Extract planes for flow visualization
+!     extract planes for flow visualization
 !
       open(11,file='bou.in',form='formatted')
       read(11,*)
@@ -34,33 +60,23 @@
       m2m=m2-1
       m3m=m3-1
 !
-!     print *,'m1m=',m1m
-!     print *,'m2m=',m2m
-!     print *,'m3m=',m3m
-!
       allocate (th(m1),rc(m2m),rm(m2m),z(m3m))
       allocate (sth(m1),cth(m1))
       allocate (rr(2*m2m))
 !
-      do i=1,m1
-       th(i)=2*pi*(i-1.d0)/m1m
-       sth(i)=sin(th(i))
-       cth(i)=cos(th(i))
+      do i = 1,m1
+       th(i) = 2*pi*(i-1.d0)/m1m
+       sth(i) = sin(th(i))
+       cth(i) = cos(th(i))
       enddo
-!
-!     print *,sth
-!     pause
-!     print *,cth
-!     pause
 !
       open(unit=98,file='radcor.out',status='unknown')
       do j=1,m2m
-!      print *,'j=',j
        read(98,*) jj,rc(j),rm(j)
       enddo
       close(98)
 !
-!     Define radial positions for theta-z planes
+!     define radial positions for theta-z planes
 !
       jplanes(1) = m2m    ! first off-wall cell
       jplanes(2) = m2m-16 ! y^+=15
@@ -74,37 +90,48 @@
       jplanes(4) = j
 !
       do j=1,2*m2m
+       jj=j-m2m
+       rr(j)=rm(jj)
        if (j.le.m2m) then
         jj=1+m2m-j
         rr(j)=-rm(jj)
-       else
-        jj=j-m2m
-        rr(j)=rm(jj)
        endif
       enddo
 !
       do k=1,m3m
        z(k)=alx3d*(k-1.d0)/m3m
       enddo
-!     print *,z
-!     pause
 !
-!     print *,'Input utau'
-!     read(*,*) utau
-!     utau = 2.1130322655568006E-002
-!
-      open(11,file='flowprop.dat',form='formatted')
-      read(11,*) aa,aa,aa,utau,aa,aa,aa,aa,aa,
-     .           aa,aa,aa,aa,aa,aa,aa,ttau,
-     .           aa,aa,aa,aa,aa,aa,aa,
-     .           aa,aa,aa,aa,aa,aa,aa,
-     .           aa,pw
-      print *,'utau,ttau,pw',utau,ttau,pw
-      close(11)
+      if (lnd.eq.3) then
+       open(11,file='flowprop.dat',form='formatted')
+       read(11,*) aa,aa,aa,utau,aa,aa,aa,aa,aa,
+     .            aa,aa,aa,aa,aa,aa,aa,ttau,
+     .            aa,aa,aa,aa,aa,aa,aa,
+     .            aa,aa,aa,aa,aa,aa,aa,
+     .            aa,pw
+       print *,'utau,ttau,pw',utau,ttau,pw
+       close(11)
+      endif
+!     read stats along r
+      nv = 30
+      allocate(wvarav(m2m,nv))
+       if (lfluc) then
+       open(11,file='stats_out.dat',action='read')
+       do j = 1,m2m
+        read(11,*) tmp,tmp,(wvarav(j,n),n=1,nv)
+       enddo
+       close(11)
+      endif
+!     set wvarav = 0 for instantaneous fields
+      if (.not.lfluc) then
+       wvarav = 0. 
+      endif
 ! 
-      utau = 0.5
-      retau=reb*utau
-!     print *,'Retau=',retau
+      if (lnd.eq.0) then
+       utau = 1.
+      elseif (lnd.eq.1) then
+       utau = .5
+      endif
 !
       allocate (planetz(nvars,nplanes,m1m,m3m))
       allocate (planetzl(m1,m3m))
@@ -112,45 +139,68 @@
       open(30,file='planetz.bin',access='stream',form='unformatted')
       read(30) planetz
       close(30)
+!     instantaneous to fluctuation
+!     pr and dens not considered
+      do lplane = 1,nplanes
+       jp = jplanes(lplane) 
+       planetz(1,lplane,1:m1m,1:m3m) = planetz(1,lplane,1:m1m,1:m3m) - 
+     .                                 wvarav(jp,1)
+       planetz(2,lplane,1:m1m,1:m3m) = planetz(2,lplane,1:m1m,1:m3m) -
+     .                                 wvarav(jp,2)
+       planetz(3,lplane,1:m1m,1:m3m) = planetz(3,lplane,1:m1m,1:m3m) -
+     .                                 wvarav(jp,3)
+      enddo
 !
-!     lplane = 3 ! index of shell to be extracted
-!
-      do lplane=1,3
+      do lplane=2,3
 !
        write(naplane,1001) lplane
  1001  format(I1.1)
 !
-       planetzl(1:m1m,:)=planetz(3,lplane,1:m1m,:)
-       planetzl(m1,:)=planetzl(1,:)
+       if (luz) then
+        planetzl(1:m1m,:)=planetz(3,lplane,1:m1m,:)
+        planetzl(m1,:)=planetzl(1,:)
+        planetzl=planetzl/utau
+        open(11,file=trim(napltz)//naplane//'.q',form='unformatted')
+        write(11) m1,1,m3m,1
+        write(11) (((planetzl(i,k),i=1,m1),j=1,1),k=1,m3m)
+        close(11)
+       endif
+!     
+       if (luth) then 
+        planetzl(1:m1m,:)=planetz(1,lplane,1:m1m,:)
+        planetzl(m1,:)=planetzl(1,:)
+        planetzl=planetzl/utau
+        open(11,file=trim(napltz)//naplane//'_uth.q',form='unformatted')
+        write(11) m1,1,m3m,1
+        write(11) (((planetzl(i,k),i=1,m1),j=1,1),k=1,m3m)
+        close(11)
+       endif
 !
-       planetzl=planetzl/utau
+       if (lur) then 
+        planetzl(1:m1m,:)=planetz(2,lplane,1:m1m,:)
+        planetzl(m1,:)=planetzl(1,:)
+        planetzl=planetzl/utau
+        open(11,file=trim(napltz)//naplane//'_ur.q',form='unformatted')
+        write(11) m1,1,m3m,1
+        write(11) (((planetzl(i,k),i=1,m1),j=1,1),k=1,m3m)
+        close(11)
+       endif
 !
-       open(11,file='planetz_'//naplane//'.q',form='unformatted')
-       rewind (11)
-       write  (11) m1,1,m3m,1
-       write  (11) (((planetzl(i,k),i=1,m1),j=1,1),k=1,m3m)
-       close(11)
-! 
        open(11,file='planetz_'//naplane//'.x',form='unformatted')
-       rewind (11)
-       write  (11) m1,1,m3m
-!      write  (11) (((rm(j)*cth(i),i=1,m1),j=m2m,m2m),k=1,m3m),
-!    .             (((rm(j)*sth(i),i=1,m1),j=m2m,m2m),k=1,m3m),
-!    .             (((z(k),i=1,m1),j=m2m,m2m),k=1,m3m) 
+       write(11) m1,1,m3m
        jj = jplanes(lplane)
-       write  (11) (((rm(j)*cth(i),i=1,m1),j=jj,jj),k=1,m3m),
-     .             (((rm(j)*sth(i),i=1,m1),j=jj,jj),k=1,m3m),
-     .             (((z(k),i=1,m1),j=jj,jj),k=1,m3m)
+       write(11) (((rm(j)*cth(i),i=1,m1),j=jj,jj),k=1,m3m),
+     .           (((rm(j)*sth(i),i=1,m1),j=jj,jj),k=1,m3m),
+     .           (((z(k),i=1,m1),j=jj,jj),k=1,m3m) 
        close(11)
 !
        open(11,file='planetz_unrolled.x',form='unformatted')
-       rewind (11)
-       write  (11) m1,1,m3m
-       write  (11) (((th(i),i=1,m1),j=m2m,m2m),k=1,m3m),
-     .             (((0.,i=1,m1),j=m2m,m2m),k=1,m3m),
-     .            (((z(k),i=1,m1),j=m2m,m2m),k=1,m3m) 
+       write(11) m1,1,m3m
+       write(11) (((th(i),i=1,m1),j=m2m,m2m),k=1,m3m),
+     .           (((0.d0,i=1,m1),j=m2m,m2m),k=1,m3m),
+     .           (((z(k),i=1,m1),j=m2m,m2m),k=1,m3m)
        close(11)
-!
+
       enddo
 !
       deallocate(planetz)
@@ -159,21 +209,53 @@
 !
       open(30,file='planerz.bin',access='stream',form='unformatted')
       read(30) planerz
-      write  (100,*) (((planerz(1,j,k),i=1,1),j=1,2*m2m),k=1,m3m)
       close(30)
+!     instantaneous to fluctuation
+!     pr and dens not considered
+      do j = 1,2*m2m
+       jj = j-m2m
+       if (j.le.m2m) then
+        jj = 1+m2m-j
+       endif
+       if (j.le.m2m) then
+        planerz(1,j,1:m3m) = planerz(1,j,1:m3m) - wvarav(jj,1)
+        planerz(2,j,1:m3m) = planerz(2,j,1:m3m) - wvarav(jj,2)
+       else
+!      inst uth, ur are -q1,-q2 on planerz
+!      discontinuous at the axis if the sign of uth/ur unchanged
+!      i.e. discontinuous at the axis if using -planerz(1,j,1:m3m)-wvarav(jj,1)
+        planerz(1,j,1:m3m) = planerz(1,j,1:m3m) + wvarav(jj,1)
+        planerz(2,j,1:m3m) = planerz(2,j,1:m3m) + wvarav(jj,2)
+       endif
+       planerz(3,j,1:m3m) = planerz(3,j,1:m3m) - wvarav(jj,3)
+      enddo
 !
       planerz=planerz/utau
 !
-      open(11,file='planerz.q',form='unformatted')
-      rewind (11)
-      write  (11) 1,2*m2m,m3m,1
-      write  (11) (((planerz(3,j,k),i=1,1),j=1,2*m2m),k=1,m3m)
-      close(11)
-! 
+      if (luz) then
+       open(11,file=trim(naplrz)//'.q',form='unformatted')
+       write(11) 1,2*m2m,m3m,1
+       write(11) (((planerz(3,j,k),i=1,1),j=1,2*m2m),k=1,m3m)
+       close(11)
+      endif
+!
+      if (luth) then 
+       open(11,file=trim(naplrz)//'_uth.q',form='unformatted')
+       write(11) 1,2*m2m,m3m,1
+       write(11) (((planerz(1,j,k),i=1,1),j=1,2*m2m),k=1,m3m)
+       close(11)
+      endif
+!
+      if (lur) then 
+       open(11,file=trim(naplrz)//'_ur.q',form='unformatted')
+       write(11) 1,2*m2m,m3m,1
+       write(11) (((planerz(2,j,k),i=1,1),j=1,2*m2m),k=1,m3m)
+       close(11)
+      endif
+
       open(11,file='planerz.x',form='unformatted')
-      rewind (11)
-      write  (11) 1,2*m2m,m3m
-      write  (11) (((rr(j)*cth(i),i=1,1),j=1,2*m2m),k=1,m3m),
+      write(11) 1,2*m2m,m3m
+      write(11) (((rr(j)*cth(i),i=1,1),j=1,2*m2m),k=1,m3m),
      .            (((rr(j)*sth(i),i=1,1),j=1,2*m2m),k=1,m3m),
      .            (((z(k),i=1,1),j=1,2*m2m),k=1,m3m)
       close(11)
@@ -187,25 +269,41 @@
       open(30,file='planetr.bin',form='unformatted')
       read(30) planetr
       close(30)
-!
-!     print *,minval(planetr(:,:,3))
-!     print *,maxval(planetr(:,:,3))
+!     instantaneous to fluctuation
+!     pr and dens not considered
+      do j = 1,m2m      
+       planetr(1:m1m,j,1) = planetr(1:m1m,j,1) - wvarav(j,1)
+       planetr(1:m1m,j,2) = planetr(1:m1m,j,2) - wvarav(j,2)
+       planetr(1:m1m,j,3) = planetr(1:m1m,j,3) - wvarav(j,3)
+      enddo
 !
       planetrl(1:m1m,:,:)=planetr(1:m1m,:,:)
       planetrl(m1,:,:)=planetrl(1,:,:)
-!
       planetrl=planetrl/utau
+      if (luz) then
+       open(11,file=trim(napltr)//'.q',form='unformatted')
+       write(11) m1,m2m,1,1
+       write(11) (((planetrl(i,j,3),i=1,m1),j=1,m2m),k=1,1)
+       close(11)
+      endif
 !
-      open(11,file='planetr.q',form='unformatted')
-      rewind (11)
-      write  (11) m1,m2m,1,1
-      write  (11) (((planetrl(i,j,3),i=1,m1),j=1,m2m),k=1,1)
-      close(11)
+      if (luth) then
+       open(11,file=trim(napltr)//'_uth.q',form='unformatted')
+       write(11) m1,m2m,1,1
+       write(11) (((planetrl(i,j,1),i=1,m1),j=1,m2m),k=1,1)
+       close(11)
+      endif
+!
+      if (lur) then
+       open(11,file=trim(napltr)//'_ur.q',form='unformatted')
+       write(11) m1,m2m,1,1
+       write(11) (((planetrl(i,j,2),i=1,m1),j=1,m2m),k=1,1)
+       close(11)
+      endif
 ! 
       open(11,file='planetr.x',form='unformatted')
-      rewind (11)
-      write  (11) m1,m2m,1
-      write  (11) (((rm(j)*cth(i),i=1,m1),j=1,m2m),k=1,1),
+      write(11) m1,m2m,1
+      write(11) (((rm(j)*cth(i),i=1,m1),j=1,m2m),k=1,1),
      .            (((rm(j)*sth(i),i=1,m1),j=1,m2m),k=1,1),
      .            (((z(k),i=1,m1),j=1,m2m),k=1,1)
       close(11)
@@ -213,4 +311,5 @@
 !    
       deallocate(planetr,planetrl)
 ! 
+      stop
       end
--- pipe/field3d/field3d.f90
+++ pipe/field3d/field3d.f90
@@ -26,11 +26,12 @@
     real(dp), allocatable, dimension(:,:,:) :: xm,ym,zm
     real(dp), allocatable, dimension(:)     :: rc,rm,zz,zzm,thetac,thetam
     real(dp), allocatable, dimension(:,:)   :: planetzl(:,:),planetz(:,:,:)
+    real(dp), allocatable, dimension(:,:)   :: planetrl(:,:),planetr(:,:)
 !    
     call MPI_INIT(ierr)
     call MPI_COMM_RANK(MPI_COMM_WORLD,nrank,ierr)
     call MPI_COMM_SIZE(MPI_COMM_WORLD,nproc,ierr)
-! 
+!
     open(11,file='bou.in')
     read(11,*)
     read(11,*) m1,m2,m3
@@ -45,16 +46,18 @@
     m3m = m3 - 1
     kstart = 1 + (m3m/nproc)*nrank 
     kend   = (m3m/nproc)*(nrank+1)
-    jplane = m2m-16
+    jplane = m2m-20
     l3d = 0
 !
     if (mod(m3m,nproc).ne.0) then
-     print *, 'error: mod(m3m,nproc).ne.0'
-     call MPI_Abort(MPI_COMM_WORLD, 1, ierr )
+      print *, 'error: mod(m3m,nproc).ne.0'
+      call MPI_Abort(MPI_COMM_WORLD, 1, ierr )
     endif
 !    
     allocate(planetz(m1m,1,1-1:m3m+1))
     allocate(planetzl(m1,m3m))
+    allocate(planetrl(m1,m2m))
+    allocate(planetr(m1m,m2m))
 !
     call h5open_f(ihdf_error)
 !
@@ -68,54 +71,71 @@
     allocate(thetam(1:m1))
 !
     do k=kstart,kend
-     zz(k) = alx3d*real(k-1,dp)/real(m3m,dp)
-     zzm(k)= alx3d*(real(k-1,dp)+0.5d0)/real(m3m,dp) 
+      zz(k) = alx3d*real(k-1,dp)/real(m3m,dp)
+      zzm(k)= alx3d*(real(k-1,dp)+0.5d0)/real(m3m,dp) 
     enddo
 !
     open(20,file='radcor.out')
     do j=1,m2m
-     read(20,*) itmp,rc(j),rm(j)
+      read(20,*) itmp,rc(j),rm(j)
     enddo
     close(20)
 !
-    pi  = 3.141592653589 
+    pi  = 3.141592653589
     dx1 = 1.0d0/(2.d0*pi/real(m1m, dp))
     do i=1,m1m
-     thetac(i) = real(i-1,dp)/dx1
-     thetam(i) = (real(i-1,dp)+0.5d0)/dx1
+      thetac(i) = real(i-1,dp)/dx1
+      thetam(i) = (real(i-1,dp)+0.5d0)/dx1
     enddo
     thetac(m1) = real(m1-1,dp)/dx1
 !
     call mpi_read_continua_surf(m1m,m2m,m3,1,m3m,jplane,3,planetz)
 !
     if (nrank.eq.0) then
-     planetzl(1:m1m,1:m3m) = planetz(1:m1m,1,1:m3m)
-     planetzl(m1,1:m3m) = planetz(1,1,1:m3m)
-     utau = 0.5
-     planetzl = planetzl/utau
-     open(11,file='planetz.q',form='unformatted')
-     write(11) m1,1,m3m,1
-     write(11) (((planetzl(i,k),i=1,m1),j=1,1),k=1,m3m)
-     close(11)
-     open(11,file='planetz.x',form='unformatted')
-     write(11) m1,1,m3m
-     jj = jplane
-!    thetac used here for consistency with extract_plane_uz.f
-     write(11) (((rm(j)*cos(thetac(i)),i=1,m1),j=jj,jj),k=1,m3m), &
-               (((rm(j)*sin(thetac(i)),i=1,m1),j=jj,jj),k=1,m3m), &
-               (((zz(k),i=1,m1),j=jj,jj),k=1,m3m)
-     close(11)
-     open(11,file='planetz_unrolled.x',form='unformatted')
-     rewind (11)
-     write  (11) m1,1,m3m
-     write  (11) (((thetac(i),i=1,m1),j=m2m,m2m),k=1,m3m), &
-                 (((0.,i=1,m1),j=m2m,m2m),k=1,m3m), &
-                 (((zz(k),i=1,m1),j=m2m,m2m),k=1,m3m)
-     close(11)
+      planetzl(1:m1m,1:m3m) = planetz(1:m1m,1,1:m3m)
+      planetzl(m1,1:m3m) = planetz(1,1,1:m3m)
+      ! utau = 1.9445827669489081E-002
+      utau = 0.5
+      planetzl = planetzl/utau
+      open(11,file='planetz.q',form='unformatted')
+      write(11) m1,1,m3m,1
+      write(11) (((planetzl(i,k),i=1,m1),j=1,1),k=1,m3m)
+      close(11)
+      open(11,file='planetz.x',form='unformatted')
+      write(11) m1,1,m3m
+      jj = jplane
+      ! thetac used here for consistency with extract_plane_uz.f
+      write(11) (((rm(j)*cos(thetac(i)),i=1,m1),j=jj,jj),k=1,m3m), &
+                (((rm(j)*sin(thetac(i)),i=1,m1),j=jj,jj),k=1,m3m), &
+                (((zz(k),i=1,m1),j=jj,jj),k=1,m3m)
+      close(11)
+      open(11,file='planetz_unrolled.x',form='unformatted')
+      rewind (11)
+      write  (11) m1,1,m3m
+      write  (11) (((thetac(i),i=1,m1),j=m2m,m2m),k=1,m3m), &
+                  (((0.,i=1,m1),j=m2m,m2m),k=1,m3m), &
+                  (((zz(k),i=1,m1),j=m2m,m2m),k=1,m3m)
+      close(11)
+  !
+      call mpi_read_continua_surf_tr(m1m,m2m,m3,1,1,1,3,planetr)
+      planetrl(1:m1m,1:m2m) = planetr(1:m1m,1:m2m)
+      planetrl(m1,1:m2m) = planetr(1,1:m2m)
+      planetrl = planetrl/utau
+      open(11,file='planetr.x',form='unformatted')
+      write(11) m1,m2m,1
+      write(11) (((rm(j)*cos(thetac(i)),i=1,m1),j=1,m2m),k=1,1), &
+                (((rm(j)*sin(thetac(i)),i=1,m1),j=1,m2m),k=1,1), &
+                (((zz(k),i=1,m1),j=1,m2m),k=1,1)
+      close(11)
+      open(11,file='planetr.q',form='unformatted')
+      write(11) m1,m2m,1,1
+      write(11) ((planetrl(i,j),i=1,m1),j=1,m2m)
+      close(11)
     end if
+!
     if (l3d.eq.0) then
-     call MPI_Barrier(MPI_COMM_WORLD)
-     stop
+      call MPI_Barrier(MPI_COMM_WORLD)
+      stop
     end if
 !
 !   continue if 3D field output
@@ -146,9 +166,9 @@
     do k=kstart,kend
     do j=1,m2m
     do i=1,m1m
-     zm(i,j,k) = zzm(k)
-     xm(i,j,k) = rm(j)*cos(thetam(i))
-     ym(i,j,k) = rm(j)*sin(thetam(i))
+      zm(i,j,k) = zzm(k)
+      xm(i,j,k) = rm(j)*cos(thetam(i))
+      ym(i,j,k) = rm(j)*sin(thetam(i))
     enddo
     enddo
     enddo
@@ -161,16 +181,16 @@
     do k = kstart,kend
     do j = 1,m2m
     do i = 1,m1m-1
-     q1m (i,j,k) = 0.5*(q1 (i,j,k) + q1 (i+1,j,k))
+      q1m (i,j,k) = 0.5*(q1 (i,j,k) + q1 (i+1,j,k))
     enddo
-     q1m (m1m,j,k) = 0.5*(q1 (m1m,j,k) + q1 (1,j,k))
+      q1m (m1m,j,k) = 0.5*(q1 (m1m,j,k) + q1 (1,j,k))
     enddo
     enddo
 !
     do k = kstart,kend
     do j = 1,m2m
     do i = 1,m1m
-     q2m (i,j,k) = 0.5*(q2 (i,j,k) + q2 (i,j+1,k))
+      q2m (i,j,k) = 0.5*(q2 (i,j,k) + q2 (i,j+1,k))
     enddo
     enddo
     enddo
@@ -178,56 +198,56 @@
     do k = kstart,kend-1
     do j = 1,m2m
     do i = 1,m1m
-     q3m (i,j,k) = 0.5*(q3 (i,j,k) + q3 (i,j,k+1))
+      q3m (i,j,k) = 0.5*(q3 (i,j,k) + q3 (i,j,k+1))
     enddo
     enddo
     enddo
     if(nrank.eq.nproc-1) then
-     do k = kend,kend
-     do j = 1,m2m
-     do i = 1,m1m
-      q3m (i,j,k) = 0.5*(q3 (i,j,k) + q3 (i,j,1))
-     enddo
-     enddo
-     enddo
+      do k = kend,kend
+      do j = 1,m2m
+      do i = 1,m1m
+        q3m (i,j,k) = 0.5*(q3 (i,j,k) + q3 (i,j,1))
+      enddo
+      enddo
+      enddo
     else
-     do k = kend,kend
-     do j = 1,m2m
-     do i = 1,m1m
-      q3m (i,j,k) = 0.5*(q3 (i,j,k) + q3 (i,j,k+1))
-     enddo
-     enddo
-     enddo
+      do k = kend,kend
+      do j = 1,m2m
+      do i = 1,m1m
+        q3m (i,j,k) = 0.5*(q3 (i,j,k) + q3 (i,j,k+1))
+      enddo
+      enddo
+      enddo
     endif
 !
     do k = kstart,kend
     do j = 1,m2m
     do i = 1,m1m
-     u1m (i,j,k) = q1m (i,j,k) 
-     u2m (i,j,k) = q2m (i,j,k)/rm(j)
-     u3m (i,j,k) = q3m (i,j,k)    
-     densm (i,j,k) = dens (i,j,k)
-     prm (i,j,k) = pr (i,j,k)    
-     uxm (i,j,k) = -sin(thetam(i))*q1m (i,j,k) &
-                   +cos(thetam(i))*q2m (i,j,k)/rm(j) 
-     uym (i,j,k) =  cos(thetam(i))*q1m (i,j,k) & 
-                   +sin(thetam(i))*q2m (i,j,k)/rm(j)
+      u1m (i,j,k) = q1m (i,j,k) 
+      u2m (i,j,k) = q2m (i,j,k)/rm(j)
+      u3m (i,j,k) = q3m (i,j,k)    
+      densm (i,j,k) = dens (i,j,k)
+      prm (i,j,k) = pr (i,j,k)    
+      uxm (i,j,k) = -sin(thetam(i))*q1m (i,j,k) &
+                    +cos(thetam(i))*q2m (i,j,k)/rm(j) 
+      uym (i,j,k) =  cos(thetam(i))*q1m (i,j,k) & 
+                    +sin(thetam(i))*q2m (i,j,k)/rm(j)
     enddo
     enddo
     enddo
     do k = kstart,kend
     do j = 1,m2m
     do i = m1,m1
-     u1m (i,j,k) = u1m (1,j,k)
-     u2m (i,j,k) = u2m (1,j,k)
-     u3m (i,j,k) = u3m (1,j,k)
-     densm (i,j,k) = densm (1,j,k)
-     prm (i,j,k) = prm (1,j,k)
-     uxm (i,j,k) = uxm (1,j,k)
-     uym (i,j,k) = uym (1,j,k)
-     xm (i,j,k) = xm (1,j,k)
-     ym (i,j,k) = ym (1,j,k)
-     zm (i,j,k) = zm (1,j,k)
+      u1m (i,j,k) = u1m (1,j,k)
+      u2m (i,j,k) = u2m (1,j,k)
+      u3m (i,j,k) = u3m (1,j,k)
+      densm (i,j,k) = densm (1,j,k)
+      prm (i,j,k) = prm (1,j,k)
+      uxm (i,j,k) = uxm (1,j,k)
+      uym (i,j,k) = uym (1,j,k)
+      xm (i,j,k) = xm (1,j,k)
+      ym (i,j,k) = ym (1,j,k)
+      zm (i,j,k) = zm (1,j,k)
     enddo
     enddo
     enddo
@@ -517,3 +537,77 @@
     call h5fclose_f(file_id, hdf_error)
     
     end subroutine mpi_read_continua_surf
+
+
+    subroutine mpi_read_continua_surf_tr(n1o,n2o,n3o,ks,ke,jplane,intvar,qua)
+    !use constants
+    !use mpih, only: MPI_COMM_WORLD,MPI_INFO_NULL
+    use hdf5  !this module contains all necessary HDF5 modules
+    implicit none
+    include 'mpif.h'
+    integer,intent(in) :: ks,ke,jplane,n2o,n1o,n3o,intvar
+    integer,parameter :: dp = 8
+    real(dp),dimension(1:n1o,1:n2o)::qua
+    integer(HID_T) :: file_id,slabspace,memspace,dset_qua,plist_id
+    integer(HSIZE_T) :: dims(3)
+    integer(HSIZE_T), dimension(3) :: data_count
+    integer(HSSIZE_T), dimension(3) :: data_offset 
+    integer :: ndims,hdf_error
+    character*70 :: filnam1
+    character*10 :: dsetname
+
+!   Select file and dataset based on intvar
+    select case (intvar)
+      case (1)
+        dsetname = trim('Vth')
+        filnam1 = trim('continua_q1.h5')
+      case (2)
+        dsetname = trim('Vr')
+        filnam1 = trim('continua_q2.h5')
+      case (3)
+        dsetname = trim('Vz')
+        filnam1 = trim('continua_q3.h5')
+      case (4)
+        dsetname = trim('dens')
+        filnam1 = trim('continua_dens.h5')
+      case (5)
+        dsetname = trim('Pr')
+        filnam1 = trim('continua_pr.h5')
+    end select
+
+!   Set offsets and element counts
+    ndims = 3
+
+    dims(1)=n1o
+    dims(2)=n2o
+    dims(3)=1
+
+    data_count(1) = n1o  
+    data_count(2) = n2o 
+    data_count(3) = 1
+
+    data_offset(1) = 0 
+    data_offset(2) = 0
+    data_offset(3) = 0
+
+    call h5pcreate_f(H5P_FILE_ACCESS_F,plist_id,hdf_error)
+    call h5pset_fapl_mpio_f(plist_id,MPI_COMM_WORLD,MPI_INFO_NULL,hdf_error)
+    call h5fopen_f(filnam1,H5F_ACC_RDONLY_F,file_id,hdf_error,access_prp=plist_id)
+    call h5dopen_f(file_id,dsetname,dset_qua,hdf_error)
+
+    call h5screate_simple_f(ndims, data_count, memspace, hdf_error) 
+
+    call h5dget_space_f(dset_qua,slabspace,hdf_error)
+    call h5sselect_hyperslab_f(slabspace,H5S_SELECT_SET_F,data_offset,data_count,hdf_error)
+    call h5pcreate_f(H5P_DATASET_XFER_F,plist_id,hdf_error) 
+    call h5pset_dxpl_mpio_f(plist_id,H5FD_MPIO_COLLECTIVE_F,hdf_error)
+    call h5dread_f(dset_qua,H5T_NATIVE_DOUBLE,&
+         qua(1:n1o,1:n2o),data_count,&
+         hdf_error,file_space_id=slabspace,mem_space_id=memspace,xfer_prp=plist_id)
+
+    call h5pclose_f(plist_id, hdf_error)
+    call h5dclose_f(dset_qua, hdf_error)
+    call h5sclose_f(memspace, hdf_error)
+    call h5fclose_f(file_id, hdf_error)
+    
+    end subroutine mpi_read_continua_surf_tr
