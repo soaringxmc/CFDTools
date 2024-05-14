5762f19cc3a613b0ad074b30655fd9daa5e96254
Soaringxmc <maochao_xiao@outlook.com>
2024-05-14 21:31:48 +0200

    forcing.out for channel-LES

--- channel-LES/single-point-stats.py
+++ channel-LES/single-point-stats.py
@@ -1,35 +1,34 @@
 import numpy as np
 import sys
 import os
+import glob
 
 sys.path.append('')
 from input import *
-import glob
-
-
-with open('input.nml', 'r') as file:
-  lines = file.readlines()
-  npoints = int(lines[1].split()[1])
-prop = np.zeros(8)
-stats = np.zeros([npoints, 10])
 
 nt = 0
 pattern = 'channel.???????.stats.txt'
-for filename in glob.glob(pattern):
-  print(filename)
-  with open(filename, 'r') as file:
-    data = file.readline().split()
-    t = float(data[1])
-    if t > tbeg and t < tend:
-      nt = nt + 1
-      prop = prop + np.array(data[1:], dtype=float)
-      stats = stats + np.loadtxt(filename, skiprows=1)
+with open('forcing.out', 'w') as fileout:
+  for filename in glob.glob(pattern):
+    with open(filename, 'r') as filein:
+      first_line = filein.readline().split()
+      fileout.write(' '.join(first_line[1:]) + '\n')
+      t = float(first_line[1])
+      if t > tbeg and t < tend:
+        nt = nt + 1
+        if(nt == 1):
+          prop = np.array(first_line[1:], dtype=float)
+          stat = np.loadtxt(filename, skiprows=1)
+        else:
+          prop = prop + np.array(first_line[1:], dtype=float)
+          stat = stat + np.loadtxt(filename, skiprows=1)
+
 prop = prop / nt
-stats = stats / nt
+stat = stat / nt
 
 retau = prop[1]
 utau  = prop[2]
 nu    = prop[3]
 delv  = nu/utau
 
-np.savetxt('stats.txt', [retau,utau,delv], fmt='%15.8f', delimiter='')
+np.savetxt('stats.txt', np.column_stack((retau, utau, delv)), fmt='%15.8f %15.8f %15.8f')
